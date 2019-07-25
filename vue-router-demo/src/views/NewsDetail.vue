5d52868f35589148e25411b1dfe5aac1287a31c2
wubh2012 <wubh2012@163.com>
2019-07-25 17:25:36 +0800

    增加 vue-router demo

---
+++ vue-router-demo/src/views/NewsDetail.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>$route.params 信息</h1>
+    <div>
+      新闻ID: {{$route.params.id}}
+      <br />
+      新闻标题: {{$route.params.title}}
+    </div>
+    <hr />
+    <h1>$route.query 信息</h1>
+    <div>
+      新闻ID: {{$route.query.id}}
+      <br />
+       新闻标题: {{$route.query.title}}
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "newsdetail"
+};
+</script>
+
