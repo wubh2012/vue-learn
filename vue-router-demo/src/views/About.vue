4ea240e06fc15f9426c739f9683e6fb27d101fcd
wubh2012 <wubh2012@163.com>
2019-07-25 17:25:36 +0800

    增加vue-router demo

---
+++ vue-router-demo/src/views/About.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>关于页面</h2>
+    <button @click="redirect">点击跳转到NewsDetail页面</button>
+  </div>
+</template>
+
+<script>
+import router from '@/router'
+export default {
+  name: 'about',
+  methods: {
+    redirect: (event)=>{
+      console.log('button click', this, event)
+      
+      console.log('router', router)
+      // router.push({ name: 'news'})
+      router.push({ name: 'newsdetail', query: { id: 100, title: '啦啦啦德玛西亚'}})
+    }
+  }
+}
+</script>
---
+++ vue-router-demo/src/views/News.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h2>这是新闻页面</h2>
+    <h3>使用 params 传递参数</h3>
+    <ul>
+      <li v-for="item in newslist" :key="item.id">
+        <router-link
+          :to="{name: 'newsdetail', params: { id: item.id, title: item.title}}"
+        >
+        {{item.title}}
+        </router-link>
+      </li>
+    </ul>
+    <h3>使用 query 传递参数</h3>
+    <ul>
+      <li v-for="item in newslist" :key="item.id">
+        <router-link
+          :to="{name: 'newsdetail', query: { id: item.id, title: item.title}}"
+        >
+        {{item.title}}
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: "news",
+  data: () => {
+    return {
+      newslist: [
+        { id: 1, title: "新闻1" },
+        { id: 2, title: "新闻2" },
+        { id: 3, title: "新闻3" },
+        { id: 4, title: "新闻4" }
+      ]
+    };
+  }
+};
+</script>
+<style scoped>
+</style>
+
+
