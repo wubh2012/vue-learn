7016a5981d0ae88426765dcbc6bbcc84da5f5e24
wubh2012 <wubh2012@163.com>
2019-07-26 14:33:17 +0800

    组件中method的this指向问题

--- vue-demo/src/views/About.vue
+++ vue-demo/src/views/About.vue
@@ -2,6 +2,13 @@
   <div>
     <h2>关于页面</h2>
     <button @click="redirect">点击跳转到NewsDetail页面</button>
+    <br>
+    <br>
+    <br>
+    <br>
+    <input type="txet" placeholder="请输入文字" v-model="msg" @keypress.enter="enter">
+    <p>{{upper}}</p>
+
   </div>
 </template>
 
@@ -9,13 +16,27 @@
 import router from '@/router'
 export default {
   name: 'about',
+  data(){
+    return {
+      msg: 'about msg'
+    }
+  },
   methods: {
-    redirect: (event)=>{
-      console.log('button click', this, event)
-      
-      console.log('router', router)
+    redirect: (event) => {
+      debugger
+      console.log('单文件组件中的this', this) // 输出 undefined
+      console.log(event)
+      // console.log('router', router)
       // router.push({ name: 'news'})
-      router.push({ name: 'newsdetail', query: { id: 100, title: '啦啦啦德玛西亚'}})
+      //router.push({ name: 'newsdetail', query: { id: 100, title: '啦啦啦德玛西亚'}})
+    },
+    enter () {
+      alert(this.msg);
+    }
+  },
+  computed: {
+    upper () {
+      return this.msg.toUpperCase();
     }
   }
 }
